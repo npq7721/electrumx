e80b56702635c18653f04f848ecf5fc82a46e11a
Tri Nguyen <tri282@gmail.com>
2019-08-28 20:03:36 -0700

    add more logging

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -56,7 +56,7 @@ class Prefetcher(object):
             except DaemonError as e:
                 self.logger.info(f'ignoring daemon error: {e}')
             except asyncio.CancelledError as e:
-                self.logger.info(f'cancelled; prefetcher stopping {e}')
+                self.logger.info(f'cancelled; prefetcher stopping with error {e}')
                 raise
             except Exception:
                 self.logger.exception(f'ignoring unexpected exception')
